3ec02218881f1a3ad6b4d41fa2082404f77f9e1e
Facassanxt <43217464+Facassanxt@users.noreply.github.com>
2020-07-21 11:34:30 +0300

    1

---
+++ Qt-WebSocket-JSON-Chat/build-ChatClient-Desktop_Qt_5_12_1_MinGW_64_bit-Debug/debug/moc_client.cpp
@@ -0,0 +1,203 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'client.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../ChatClient/client.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'client.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Client_t {
+    QByteArrayData data[10];
+    char stringdata0[123];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Client_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Client_t qt_meta_stringdata_Client = {
+    {
+QT_MOC_LITERAL(0, 0, 6), // "Client"
+QT_MOC_LITERAL(1, 7, 14), // "receiveMessage"
+QT_MOC_LITERAL(2, 22, 0), // ""
+QT_MOC_LITERAL(3, 23, 7), // "message"
+QT_MOC_LITERAL(4, 31, 15), // "requestUsername"
+QT_MOC_LITERAL(5, 47, 15), // "receiveUserlist"
+QT_MOC_LITERAL(6, 63, 5), // "array"
+QT_MOC_LITERAL(7, 69, 21), // "receiveServiceMessage"
+QT_MOC_LITERAL(8, 91, 16), // "onReceiveMessage"
+QT_MOC_LITERAL(9, 108, 14) // "onDisconnected"
+
+    },
+    "Client\0receiveMessage\0\0message\0"
+    "requestUsername\0receiveUserlist\0array\0"
+    "receiveServiceMessage\0onReceiveMessage\0"
+    "onDisconnected"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Client[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       6,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       4,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   44,    2, 0x06 /* Public */,
+       4,    0,   47,    2, 0x06 /* Public */,
+       5,    1,   48,    2, 0x06 /* Public */,
+       7,    1,   51,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       8,    1,   54,    2, 0x08 /* Private */,
+       9,    0,   57,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QJsonArray,    6,
+    QMetaType::Void, QMetaType::QString,    3,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Client::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Client *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->receiveMessage((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 1: _t->requestUsername(); break;
+        case 2: _t->receiveUserlist((*reinterpret_cast< QJsonArray(*)>(_a[1]))); break;
+        case 3: _t->receiveServiceMessage((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 4: _t->onReceiveMessage((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 5: _t->onDisconnected(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (Client::*)(QString );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Client::receiveMessage)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (Client::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Client::requestUsername)) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            using _t = void (Client::*)(QJsonArray );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Client::receiveUserlist)) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            using _t = void (Client::*)(QString );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Client::receiveServiceMessage)) {
+                *result = 3;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject Client::staticMetaObject = { {
+    &QObject::staticMetaObject,
+    qt_meta_stringdata_Client.data,
+    qt_meta_data_Client,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Client::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Client::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Client.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int Client::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 6)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 6;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void Client::receiveMessage(QString _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void Client::requestUsername()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+
+// SIGNAL 2
+void Client::receiveUserlist(QJsonArray _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 2, _a);
+}
+
+// SIGNAL 3
+void Client::receiveServiceMessage(QString _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 3, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
